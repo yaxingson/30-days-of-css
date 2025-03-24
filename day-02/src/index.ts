536b4b7b2a6abd23e91bb3aded7ff500acb8f2e4
soetas <3228891558@qq.com>
2025-03-24 10:48:04 +0800

    feat: init

--- day-02/src/index.ts
+++ day-02/src/index.ts
@@ -2,7 +2,7 @@ import { mount } from 'svelte'
 import BasicSyntax from './BasicSyntax.svelte'
 
 const app = mount(BasicSyntax, {
-  target:document.getElementById('app')
+  target:document.getElementById('app')!
 })
 
 export default app
